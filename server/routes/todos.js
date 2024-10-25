9b45e7bc763fb1b2ab5f766baa1441aec97589ec
Edge-html <solanaian53@gmail.com>
2024-10-25 22:36:07 +0800

    Todo routes update and Todo schema and models

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -5,13 +5,12 @@ import Todo from "../models/todo.js";
 const router = express.Router();
 
 router.get("/todos", async (req, res) => {
-  // Fetch all todos and populate the 'user' field to include user details
+
   const todos = await Todo.find().populate('user');
   res.json(todos);
 });
 
 router.get("/todos/:id", async (req, res) => {
-  // Fetch a single todo by id and populate the 'user' field
   const todo = await Todo.findById(req.params.id).populate('user');
   if (todo) {
     res.json(todo);
@@ -23,18 +22,17 @@ router.get("/todos/:id", async (req, res) => {
 router.post("/todos", async (req, res) => {
   const { user, title, description } = req.body;
 
-  // Check for required fields
   if (!user || !title) {
     return res.status(400).json({ message: "User and title are required." });
   }
 
   try {
-    // Create and save the new todo
+   
     const todo = new Todo({ user, title, description });
     await todo.save();
     res.status(201).json(todo);
   } catch (error) {
-    // Handle any other errors
+ 
     res.status(500).json({ message: error.message });
   }
 });
@@ -43,7 +41,6 @@ router.put("/todos/:id", async (req, res) => {
   const { title, description } = req.body;
 
   try {
-    // Update the todo by id
     const todo = await Todo.findByIdAndUpdate(req.params.id, { title, description }, { new: true });
     if (todo) {
       res.json(todo);
@@ -57,7 +54,6 @@ router.put("/todos/:id", async (req, res) => {
 
 router.delete("/todos/:id", async (req, res) => {
   try {
-    // Delete the todo by id
     const result = await Todo.findByIdAndDelete(req.params.id);
     if (result) {
       res.send(`Deleted Todo with id ${req.params.id}`);
